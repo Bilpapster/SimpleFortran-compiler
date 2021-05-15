86f4f56ac766be03b9d584527bef4d1e6afbe05d
Vasileios Papastergios <papastva@csd.auth.gr>
2021-05-15 21:33:40 +0300

    Created and integrated a separate class for semantic errors handling.

---
+++ src/main/java/Listeners/SemanticErrorsHandler.java
@@ -0,0 +1,23 @@
+package Listeners;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SemanticErrorsHandler {
+    private static final List<String> semanticErrors = new ArrayList<>();
+
+    public static void addError(String errorDescription) {
+        semanticErrors.add(errorDescription);
+    }
+
+    public static boolean hasErrors() {
+        return !semanticErrors.isEmpty();
+    }
+
+    public static void printErrors() {
+        System.out.println("The program could not compile, due to the following error(s):");
+        for (String semanticError : semanticErrors) {
+            System.err.println(semanticError);
+        }
+    }
+}
