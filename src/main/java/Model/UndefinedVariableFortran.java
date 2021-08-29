1bc198145b50c3934baeea7ceb61e6b4086dd581
Vasileios Papastergios <papastva@csd.auth.gr>
2021-08-29 18:11:12 +0300

    Added two simple getters.

--- src/main/java/Model/UndefinedVariableFortran.java
+++ src/main/java/Model/UndefinedVariableFortran.java
@@ -1,11 +1,19 @@
 package Model;
 
-public abstract class UndefinedVariableFortran extends ASTNodeFortran{
+public abstract class UndefinedVariableFortran extends ASTNodeFortran {
     protected String name;
     protected boolean isList;
 
+    public String getName() {
+        return this.name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean getIsList() {
+        return this.isList;
     }
 
     public void setIsList(boolean isList) {
