71fa0d73f6d967d6c6b08fdf87af2df226486872
Vasileios Papastergios <papastva@csd.auth.gr>
2021-09-05 20:05:41 +0300

    Initial commit and basic code for the auxiliary class that represents an array-info-carrier.

---
+++ src/main/java/Model/FunctionSpecificationFortran.java
@@ -0,0 +1,53 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class FunctionSpecificationFortran {
+    private final ArrayList<DataTypeFortran> parameters = new ArrayList<>();
+    private DataTypeFortran returnType = null;
+
+    public FunctionSpecificationFortran(DataTypeFortran returnType) {
+        this.returnType = returnType;
+    }
+
+    public FunctionSpecificationFortran() {
+        // intentionally left blank
+    }
+
+    public DataTypeFortran getReturnType() {
+        return returnType;
+    }
+
+    public void setReturnType(DataTypeFortran returnType) {
+        this.returnType = returnType;
+    }
+
+    public void addParameter(DataTypeFortran parameter) {
+        this.parameters.add(parameter);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof FunctionSpecificationFortran)) {
+            return false;
+        }
+
+        FunctionSpecificationFortran other = (FunctionSpecificationFortran) obj;
+
+        if (parameters.size() != other.parameters.size()) {
+            return false;
+        }
+
+        for (int parameterIndex = 0; parameterIndex < parameters.size(); parameterIndex++) {
+            if (this.parameters.get(parameterIndex) != other.parameters.get(parameterIndex)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
